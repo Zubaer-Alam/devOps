3c56556f78fbfb8ee18ab711082f9cf8dee32410
zubaer <xuvaer@gmail.com>
2021-12-01 12:25:46 +0600

    initialise

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 
 app.get ('/',(req,res)=>{
-    res.send('Hello World')
+    res.send('Hello')
 })
+const PORT = 2051
 
-app.listen(2051,()=>{
-    console.log("server is running on port 2051");
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
 })
